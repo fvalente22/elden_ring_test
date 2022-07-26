0efe268d769b463c148c722922f1228039e0d425
fvalente22 <55769176+fvalente22@users.noreply.github.com>
2022-07-26 14:05:23 -0700

    first elden ring push

---
+++ app.js
@@ -0,0 +1,20 @@
+/**
+ * handleClick opens link in a new window
+ */
+
+const handleClick = () => {
+    window.open(
+      'https://calendly.com/trust-and-will/15min-trust-will-support',
+      '_blank'
+    );
+  }
+//   this was for the button testing but leaving here in case
+
+/**
+ * when clicked it will return value of X
+ * @param {string} value 
+ * @returns 
+ */
+const handleClick = (value) => {
+    return value
+  }
